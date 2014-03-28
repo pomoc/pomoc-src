b02c74fed915b0f48ef306547ebd8ab3b2c77dbc
Steve <steveng.1988@gmail.com>
2014-03-29 00:58:22 +0800

    [Soedar]Added possible delegate and methods of PMSupport needed

---
+++ Dashboard/Pomoc Dashboard Client/SoedarPMSupport.m
@@ -0,0 +1,13 @@
+//
+//  SoedarPMSupport.m
+//  Pomoc Dashboard Client
+//
+//  Created by Steve Ng on 29/3/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import "SoedarPMSupport.h"
+
+@implementation SoedarPMSupport
+
+@end
