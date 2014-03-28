4ed8b2dcc02c5c12d3312591342879a1368dc0d4
Chunmun <xilasirc@gmail.com>
2014-03-28 08:26:49 +0800

    Initial Commit

---
+++ Annotator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Annotator
+//
+//  Created by Soon Chun Mun on 28/3/14.
+//  Copyright (c) 2014 Soon Chun Mun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Annotator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Annotator
+//
+//  Created by Soon Chun Mun on 28/3/14.
+//  Copyright (c) 2014 Soon Chun Mun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
