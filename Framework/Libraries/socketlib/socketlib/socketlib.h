d538083518985a17027a39694cb8b3523b523e57
soedar <soedar.sg@gmail.com>
2014-04-12 01:10:27 +0800

    Initial Commit

---
+++ Framework/Libraries/socketlib/socketlib/socketlib.h
@@ -0,0 +1,13 @@
+//
+//  socketlib.h
+//  socketlib
+//
+//  Created by soedar on 12/4/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface socketlib : NSObject
+
+@end
---
+++ Framework/Libraries/socketlib/socketlib/socketlib.m
@@ -0,0 +1,13 @@
+//
+//  socketlib.m
+//  socketlib
+//
+//  Created by soedar on 12/4/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import "socketlib.h"
+
+@implementation socketlib
+
+@end
