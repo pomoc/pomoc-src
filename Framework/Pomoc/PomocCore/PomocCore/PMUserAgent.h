6e189f7ef96f5a2c3d2e9f348b5ed04bd4077427
banghui <limbanghui@gmail.com>
2014-04-11 10:46:48 +0800

    init user types

---
+++ Framework/Pomoc/PomocCore/PomocCore/PMUserAgent.h
@@ -0,0 +1,13 @@
+//
+//  PMUserAgent.h
+//  PomocCore
+//
+//  Created by Bang Hui Lim on 4/11/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import "PMUser.h"
+
+@interface PMUserAgent : PMUser
+
+@end
---
+++ Framework/Pomoc/PomocCore/PomocCore/PMUserPublic.m
@@ -0,0 +1,13 @@
+//
+//  PMUserPublic.m
+//  PomocCore
+//
+//  Created by Bang Hui Lim on 4/11/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import "PMUserPublic.h"
+
+@implementation PMUserPublic
+
+@end
