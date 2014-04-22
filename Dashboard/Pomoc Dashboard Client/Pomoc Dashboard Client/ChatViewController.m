6fc1c7a26965385002777ef9180d3bea32756da6
Steve <steveng.1988@gmail.com>
2014-04-22 16:28:20 +0800

    smoothen the cell

--- Dashboard/Pomoc Dashboard Client/Pomoc Dashboard Client/ChatViewController.m
+++ Dashboard/Pomoc Dashboard Client/Pomoc Dashboard Client/ChatViewController.m
@@ -211,6 +211,7 @@
         if ([subviews[i] isKindOfClass:[UIImageView class]] &&
             ((UIImageView *)subviews[i]).image) {
             NSLog(@"met criteria!");
+            
             imv = subviews[i];
             break;
         }
@@ -534,18 +535,74 @@
     [message retrieveImageWithCompletion:^(UIImage *image) {
         
         NSLog(@"image retrieved");
-        weakCell.messagePicture.contentMode = UIViewContentModeScaleAspectFill;
+        weakCell.messagePicture.contentMode = UIViewContentModeScaleAspectFit;
         weakCell.messagePicture.clipsToBounds = YES;
-        UIImage *scaled = [Utility scaleImage:image toSize:CGSizeMake(120, 120)];
-        [weakCell.messagePicture setImage:scaled];
+        //UIImage *scaled = [Utility scaleImage:image toSize:CGSizeMake(120, 120)];
         
-        [weakCell.messageBigPicture setImage:image];
-        weakCell.messageBigPicture.hidden = true;
+        [weakCell.messagePicture setImage:image];
+        
+        UITapGestureRecognizer *tapRecognizer =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapHandler:)];
+        [weakCell.messagePicture addGestureRecognizer:tapRecognizer];
+        
+        //[weakCell.messageBigPicture setImage:image];
+        //weakCell.messageBigPicture.hidden = true;
         [weakCell setNeedsLayout];
         
     }];
+    
+    
+    
     return cell;
 }
+
+- (IBAction)tapHandler:(UITapGestureRecognizer *)recognizer
+{
+    NSLog(@"image clicked");
+}
+
+/*
+ 
+ 
+ 
+ */
+
+
+#define CHAT_VIEW_HEADER_HEIGHT 100 
+#define CHAT_VIEW_HEADER_HEIGHT 500
+//
+//- (void)showImage:(UIImage *)image
+//{
+//    self.imageView = [[UIView alloc] initWithFrame:CGRectMake(0, CHAT_VIEW_HEADER_HEIGHT, self.frame.size.width, self.frame.size.height-CHAT_VIEW_HEADER_HEIGHT)];
+//    [self.imageView setBackgroundColor:[UIColor colorWithWhite:0 alpha:0.3]];
+//    
+//    CGRect frame = self.imageView.bounds;
+//    frame.size.width *= 0.9;
+//    frame.size.height *= 0.9;
+//    
+//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:frame];
+//    [imageView setImage:image];
+//    [imageView setCenter:CGPointMake(self.imageView.frame.size.width/2.0, self.imageView.frame.size.height / 2.0)];
+//    [imageView setContentMode:UIViewContentModeScaleAspectFit];
+//    [self.imageView addSubview:imageView];
+//    
+//    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(removeImage:)];
+//    [self.imageView addGestureRecognizer:tapGestureRecognizer];
+//    
+//    UIImageView *closeImage = [[UIImageView alloc] initWithFrame:CGRectMake(self.imageView.frame.size.width-32, 0, 32, 32)];
+//    
+//    [closeImage setImage:[PomocResources imageNamed:@"close-icon" type:@"png"]];
+//    
+//    [self.imageView addSubview:closeImage];
+//    
+//    [self addSubview:self.imageView];
+//}
+//
+//- (void)removeImage:(UITapGestureRecognizer *)tapRecognizer
+//{
+//    [self.imageView removeFromSuperview];
+//}
+
+
 
 - (UITableViewCell *) createStatusTableView: (UITableView *)tableView atRow: (NSInteger)row
 {
